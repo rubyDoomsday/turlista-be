200d08a99f65bdf244a2c924989b5f59980b3d3f
Rebecca Chapin <rebecca.chapin@validic.com>
2020-02-16 22:38:32 -0500

    Events CRUD
    **Description**
    This update adds routes and controllers for trip events CRUD in addition
    to the new ability to show trips by ID.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show create update destroy] do
     resources :trips, only: %i[index show create update destroy]
+  end
+
+  resources :trips, only: %i[show] do
+    resources :events, only: %i[index show create update destroy]
   end
 end
